4a40f20f41be314af8654c6d8d5d230074777e9e
William Goode <wgoode3@gmail.com>
2020-06-17 19:46:56 -0400

    I did some css-ing

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,11 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+
+// attach validation error styling to any input that is invalid
+$(".input-validation-error").each(function(){
+    $(this).addClass("is-invalid");
+});
+
+// autofocus the first invalid input 
+$(".input-validation-error").first().focus();
